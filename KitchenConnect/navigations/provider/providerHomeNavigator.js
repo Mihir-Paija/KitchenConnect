22f80cfe8feeb195e51dec140cb54ebe298b1b8d
Dhruv Shah <94975058+DhruvShah87@users.noreply.github.com>
2024-05-20 23:29:18 +0530

    Added Navigation in Provider Home Screen

--- KitchenConnect/navigations/provider/providerHomeNavigator.js
+++ KitchenConnect/navigations/provider/providerHomeNavigator.js
@@ -1,33 +1,53 @@
 import React, { useContext } from "react";
-import { createStackNavigator, TransitionSpecs } from "@react-navigation/stack";
-import HomeScreen from "@/screens/provider/homeCustomerScreen";
+import { Image } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { UserTypeContext } from "../../context/userTypeContext";
+import MenuScreen from "@/screens/provider/menuScreen";
+import OrdersScreen from "@/screens/provider/ordersScreen";
+import SubscriptionsScreen from "@/screens/provider/subscriptionsScreen";
+import ProfileScreen from "@/screens/provider/profileScreen";
+import icons from "@/utils/customerIconpaths"
 
-const homeStack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
-const ProviderHomeNavigator = ({ route }) => {
+const ProviderHomeNavigator = () => {
   //global states
   const [userType] = useContext(UserTypeContext);
 
   return (
-    <homeStack.Navigator
-      initialRouteName="Provider Home"
-      screenOptions={{
-        headerShown: false,
-        gestureEnabled: true,
-        gestureDirection: "horizontal",
-        transitionSpec: {
-          open: TransitionSpecs.TransitionIOSSpec,
-          close: TransitionSpecs.TransitionIOSSpec,
-        },
-      }}
-    >
-      <homeStack.Screen
-        name="Provider Home"
-        component={HomeScreen}
-        options={{ presentation: "transparentModal" }}
-      />
-    </homeStack.Navigator>
+    <Tab.Navigator
+    initialRouteName="Provider Menu"
+    screenOptions={ ({ route }) => ({
+      headerShown: false,
+      gestureEnabled: true,
+      tabBarIcon: ({ focused }) => {
+        let iconSource;
+        switch (route.name) {
+          case 'Provider Menu':
+            iconSource = focused ? icons.Tiffin.active : icons.Tiffin.inactive;
+            break;
+          case 'Orders':
+            iconSource = focused ? icons.History.active : icons.History.inactive;
+            break;
+          case 'Subscriptions':
+            iconSource = focused ? icons.Wallet.active : icons.Wallet.inactive;
+            break;
+          case 'Profile':
+            iconSource = focused ? icons.Profile.active : icons.Profile.inactive;
+            break;
+        }
+        return <Image source={iconSource} style={{ width: 25, height: 25 }} />;
+      },
+      tabBarActiveTintColor: '#ffa500',
+      tabBarInactiveTintColor: 'gray',
+      
+    })}
+  >
+    <Tab.Screen name="Menu" component={MenuScreen} />
+    <Tab.Screen name="Orders" component={OrdersScreen} />
+    <Tab.Screen name="Subscriptions" component={SubscriptionsScreen} />
+    <Tab.Screen name="Profile" component={ProfileScreen} />
+  </Tab.Navigator>
   );
 };
 
--- KitchenConnect/screens/shared/loginScreen.js
+++ KitchenConnect/screens/shared/loginScreen.js
@@ -82,7 +82,8 @@ const LoginScreen = ({ navigation }) => {
         {
           const responseData = await loginProvider(bodyData);
           setAuth(responseData, bodyData)
-          navigation.navigate("Provider Home");
+          //navigation.navigate("Provider Home");
+          navigation.navigate('Provider Home', { screen: 'Menu' })
           console.log("Provider login data => " + JSON.stringify(bodyData));
         }
       }
