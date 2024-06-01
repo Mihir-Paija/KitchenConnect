efdacda532ba83868f9167af81624f0f7d56d022
Dhruv Shah <94975058+DhruvShah87@users.noreply.github.com>
2024-06-01 16:41:53 +0530

    Created Model and Routes for Menu

---
+++ server/apps/models/menuModel.js
@@ -0,0 +1,55 @@
+import mongoose from 'mongoose';
+const { Schema, model } = mongoose;
+
+const menuDetails = new mongoose.Schema({
+    itemType: {
+        type: String,
+        required: [true, "Please Provide Item Type"]
+    },
+
+    itemName: {
+        type: String,
+        required: [true, "Please Provide Item Name"]
+    },
+
+    quantity: {
+        type: Number,
+        required: [true, "Please Provide Quantity"]
+    },
+
+    unit: {
+        type: String,
+        required: [true, "Please Provide Unit"]
+    }
+});
+
+const menuSchema = new Schema ({
+    providerID: {
+        type: Schema.Types.ObjectId,
+        ref: 'Provider',  
+        required: true
+    },
+    
+    tiffinID: {
+        type: Schema.Types.ObjectId,
+        ref: 'Tiffins',  
+        required: true
+    },
+
+    day: {
+        type: String,
+        enum: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'], 
+        required: [true, "Please Enter The Day"],
+        unique: [true, "This day already exists"],
+    },
+
+    menu: {
+        type: menuDetails,
+        required: [true],
+    }
+
+}, {timestamps: true})
+
+const menu = mongoose.model('Menu', menuSchema);
+
+export default menu;
--- server/apps/models/providerModel.js
+++ server/apps/models/providerModel.js
@@ -2,64 +2,64 @@ import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
 
 const addressSchema = new mongoose.Schema({
-    flatNumber: {
-      type: String,
-      required: [true, "Please Enter Flat Number"]
-    },
-    street: {
-      type: String,
-      required: [true, "Please Enter Street"]
-    },
-    landmark: {
-      type: String,
-      required: false
-    }
+  flatNumber: {
+    type: String,
+    required: [true, "Please Enter Flat Number"]
+  },
+  street: {
+    type: String,
+    required: [true, "Please Enter Street"]
+  },
+  landmark: {
+    type: String,
+    required: false
+  }
 });
 
 const providerSchema = new Schema({
-    name: {
-      type: String,
-      required: [true, "Please Enter Your Name"],
-    },
-    email: {
-      type: String,
-      required: [true, "Please Enter Your Email-ID"],
-      unique: [true, "This email address is already exists"],
-    },
-    mobile: {
-      type: String,
-      required: [true, "Please Enter Your Mobile Number"],
-    },
-    password: {
-      type: String,
-      required: [true, "Please Create A Password"],
-    },
-    city: {
-      type: String,
-      required: [true, "Please Enter Your Business City"]
-    },
-    kitchenName: {
-      type: String,
-      required: [true, "Please Enter Your Business Name"]
-    },
-    shortDescription: {
-      type: String,
-      required: [true, "Please Enter A Short Description of your Business"]
-    },
-    address: [addressSchema],
-    basePrice: {
-      type: Number,
-      default: 0,
-    },
-    provideDelivery: {
-      type: Boolean,
-      required: [true, "Please Enter Delivery Mode"]
-    },
-    
-    deactivate: {
-      type: Boolean,
-      default: false,
-    }
+  name: {
+    type: String,
+    required: [true, "Please Enter Your Name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please Enter Your Email-ID"],
+    unique: [true, "This email address is already exists"],
+  },
+  mobile: {
+    type: String,
+    required: [true, "Please Enter Your Mobile Number"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please Create A Password"],
+  },
+  city: {
+    type: String,
+    required: [true, "Please Enter Your Business City"]
+  },
+  kitchenName: {
+    type: String,
+    required: [true, "Please Enter Your Business Name"]
+  },
+  shortDescription: {
+    type: String,
+    required: [true, "Please Enter A Short Description of your Business"]
+  },
+  address: [addressSchema],
+  basePrice: {
+    type: Number,
+    default: 0,
+  },
+  provideDelivery: {
+    type: Boolean,
+    required: [true, "Please Enter Delivery Mode"]
+  },
+
+  deactivate: {
+    type: Boolean,
+    default: false,
+  }
 }, { timestamps: true });
 
 const provider = mongoose.model('Provider', providerSchema);
