5699f428d3e1ee8888b9031d33a61d41be53464f
Dhruv Shah <94975058+DhruvShah87@users.noreply.github.com>
2024-06-05 16:54:56 +0530

    Modified Add and Edit Tiffin Modals

---
+++ KitchenConnect/screens/provider/addTiffinModal1.js
@@ -0,0 +1,230 @@
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  Modal,
+  StyleSheet,
+  Picker,
+  ScrollView,
+  Alert
+} from "react-native";
+import { windowWidth, windowHeight } from "@/utils/dimensions";
+import RNPickerSelect from "react-native-picker-select";
+import CheckBox from "react-native-check-box";
+
+const AddTiffinModal1 = ({ isVisible, onClose, onNext }) => {
+  const [tiffinData, setTiffinData] = useState({
+    name: "",
+    shortDescription: "",
+    price: "",
+    
+  });
+
+  const handleNext = () => {
+    if(!tiffinData.name || !tiffinData.shortDescription || !tiffinData.price){
+      Alert.alert("Please Fill All Fields");
+      return
+    }
+    onNext(tiffinData);
+  };
+
+  const foodTypeOptions = [
+    "Veg",
+    "Non-Veg",
+    "Swaminarayan",
+    "Jain",
+    "Vegan",
+  ].map((value) => ({ label: value, value: value }));
+  const tiffinTypeOptions = ["Lunch", "Dinner"].map((value) => ({
+    label: value,
+    value: value,
+  }));
+  const hourOptions = Array.from({ length: 24 }, (_, i) => ({
+    label: i.toString().padStart(2, "0"),
+    value: i.toString().padStart(2, "0"),
+  }));
+  const minuteOptions = ["00", "15", "30", "45"].map((value) => ({
+    label: value,
+    value: value,
+  }));
+
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={isVisible}
+      onRequestClose={onClose}
+    >
+      <View style={styles.modalContainer}>
+        <View style={styles.modalContent}>
+          <ScrollView>
+            <Text style={styles.modalTitle}>Add New Tiffin</Text>
+            <TextInput
+              style={styles.input}
+              placeholder={tiffinData.name ? tiffinData.name : "Name"}
+              value={tiffinData.name}
+              onChangeText={(text) =>
+                setTiffinData({ ...tiffinData, name: text })
+              }
+            />
+            <TextInput
+              style={[styles.input, { height: 80 }]}
+              placeholder="Write A Short Description"
+              value={tiffinData.shortDescription}
+              onChangeText={(text) =>
+                setTiffinData({ ...tiffinData, shortDescription: text })
+              }
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Enter Tiffin Price"
+              value={tiffinData.price}
+              onChangeText={(text) =>
+                setTiffinData({ ...tiffinData, price: text })
+              }
+              keyboardType="numeric"
+            />
+                    </ScrollView>
+                    <View style = {styles.btnContainer}>
+                    <TouchableOpacity style={styles.submitButton} onPress={handleNext}>
+                        <Text style={styles.buttonText}>Next</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.closeButton} onPress={onClose}>
+                        <Text style={styles.buttonText}>Close</Text>
+                    </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+        </Modal>
+    );
+};
+
+export default AddTiffinModal1;
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+
+    modalContent: {
+        backgroundColor: '#fff',
+        borderTopLeftRadius: windowWidth * 0.05,
+        borderTopRightRadius: windowWidth * 0.05,
+        padding: windowWidth * 0.03,
+        width: '100%',
+    },
+
+    modalTitle: {
+        fontSize: windowWidth * 0.06,
+        fontWeight: 'bold',
+        marginBottom: windowHeight * 0.01,
+    },
+
+    input: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: windowWidth * 0.02,
+        paddingVertical: windowHeight * 0.01,
+        paddingHorizontal: windowWidth * 0.03,
+        marginBottom: windowHeight * 0.01,
+    },
+
+    pickerContainer: {
+        marginBottom: windowHeight * 0.01,
+    },
+    
+    pickerContainerHalf: {
+        flex: 1,
+        marginRight: windowWidth * 0.01,  
+    },
+    
+    pickerContainerHalfLast: {
+        flex: 1,
+        marginRight: 0,  
+    },
+
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: windowHeight * 0.011,
+    },
+
+    label: {
+        marginBottom: windowHeight * 0.005,
+        fontSize: windowWidth * 0.04,
+    },
+
+    checkboxContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: windowHeight * 0.02,
+    },
+    checkbox: {
+        padding: 0,
+        margin: 0,
+    },
+    labels: {
+        fontSize: windowWidth * 0.04,
+        marginLeft: windowWidth * 0.025,
+    },
+
+    btnContainer: {
+        alignItems: 'center'
+    },
+
+    submitButton: {
+        backgroundColor: 'green',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: windowHeight * 0.05,
+        width: windowWidth * 0.95,
+        borderRadius: windowWidth * 0.02,
+        marginTop: windowHeight * 0.01,
+    },
+
+    closeButton: {
+        backgroundColor: 'red',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: windowHeight * 0.05,
+        width: windowWidth * 0.95,
+        borderRadius: windowWidth * 0.02,
+        marginTop: windowHeight * 0.01,
+    },
+
+    buttonText: {
+        color: '#fff',
+        fontSize: windowWidth * 0.04,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+});
+
+const pickerSelectStyles = StyleSheet.create({
+    inputIOS: {
+        fontSize: windowWidth * 0.04,
+        paddingVertical: windowHeight * 0.01,
+        paddingHorizontal: windowWidth * 0.03,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: windowWidth * 0.02,
+        color: 'black',
+        paddingRight: windowWidth * 0.1,
+        marginBottom: windowHeight * 0.01,
+    },
+    inputAndroid: {
+        fontSize: windowWidth * 0.035,
+        paddingHorizontal: windowWidth * 0.03,
+        paddingVertical: windowHeight * 0.01,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: windowWidth * 0.02,
+        color: 'black',
+        paddingRight: windowWidth * 0.1,
+        marginBottom: windowHeight * 0.01,
+    },
+});
--- KitchenConnect/screens/provider/editTiffinModal2.js
+++ KitchenConnect/screens/provider/editTiffinModal2.js
@@ -1,126 +1,87 @@
-import React, { useState, useEffect } from "react";
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  TextInput,
-  Modal,
-  StyleSheet,
-  Picker,
-  ScrollView,
-} from "react-native";
-import { windowWidth, windowHeight } from "@/utils/dimensions";
-import RNPickerSelect from "react-native-picker-select";
-import CheckBox from "react-native-check-box";
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, TextInput, Modal, StyleSheet, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
+import { windowWidth, windowHeight } from '@/utils/dimensions';
+import RNPickerSelect from 'react-native-picker-select';
+import CheckBox from 'react-native-check-box';
+import Icon from "react-native-vector-icons/Ionicons";
 
-const AddTiffinModal = ({ isVisible, onClose, onAddTiffin }) => {
-  const [tiffinData, setTiffinData] = useState({
-    name: "",
-    shortDescription: "",
-    price: "",
-    foodType: "Veg", // Default value
-    tiffinType: "Lunch",
-    hours: "",
-    mins: "",
-    availability: false,
-    deliveryCharge: 0,
-    deliveryTimeHrs: "",
-    deliveryTimeMins: "",
-  });
+const EditTiffinModal2 = ({ isVisible, item, onBack, onClose, onEditTiffin}) => {
 
-  const handleAddTiffin = () => {
-    onAddTiffin(tiffinData);
-
-    setTiffinData({
-      name: "",
-      shortDescription: "",
-      price: "",
-      foodType: "Veg",
-      tiffinType: "Lunch",
-      hours: "",
-      mins: "",
-      availability: false,
-      deliveryCharge: 0,
-      deliveryTimeHrs: "",
-      deliveryTimeMins: "",
+    const [tiffinData, setTiffinData] = useState({
+        id: item.id,
+        name: item.name || '',
+        shortDescription: item.shortDescription || '',
+        price: item.price ? item.price.toString() : '',
+        foodType: item.foodType || 'Veg',
+        tiffinType: item.tiffinType || 'Lunch',
+        hours: item.hours || '',
+        mins: item.mins || '',
+        availability: item.deliveryDetails.availability || false,
+        deliveryCharge: item.deliveryDetails.availability ? item.deliveryDetails.deliveryCharge ? item.deliveryDetails.deliveryCharge.toString() : '0' : '0',
+        deliveryTimeHrs: item.deliveryDetails.availability ? item.deliveryDetails.deliveryTimeHrs : '',
+        deliveryTimeMins: item.deliveryDetails.availability ? item.deliveryDetails.deliveryTimeMins : ''
     });
-  };
 
-  const foodTypeOptions = [
-    "Veg",
-    "Non-Veg",
-    "Swaminarayan",
-    "Jain",
-    "Vegan",
-  ].map((value) => ({ label: value, value: value }));
-  const tiffinTypeOptions = ["Lunch", "Dinner"].map((value) => ({
-    label: value,
-    value: value,
-  }));
-  const hourOptions = Array.from({ length: 24 }, (_, i) => ({
-    label: i.toString().padStart(2, "0"),
-    value: i.toString().padStart(2, "0"),
-  }));
-  const minuteOptions = ["00", "15", "30", "45"].map((value) => ({
-    label: value,
-    value: value,
-  }));
+    const handleEditTiffin = () => {
+        if(!tiffinData.name || !tiffinData.shortDescription || !tiffinData.price || !tiffinData.foodType || !tiffinData.tiffinType || !tiffinData.hours || !tiffinData.mins){
+            Alert.alert("Please Fill All Fields");
+            return
+          }
 
-  return (
-    <Modal
-      animationType="slide"
-      transparent={true}
-      visible={isVisible}
-      onRequestClose={onClose}
-    >
-      <View style={styles.modalContainer}>
-        <View style={styles.modalContent}>
-          <ScrollView>
-            <Text style={styles.modalTitle}>Add New Tiffin</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Name"
-              value={tiffinData.name}
-              onChangeText={(text) =>
-                setTiffinData({ ...tiffinData, name: text })
-              }
-            />
-            <TextInput
-              style={[styles.input, { height: 80 }]}
-              placeholder="Write A Short Description"
-              value={tiffinData.shortDescription}
-              onChangeText={(text) =>
-                setTiffinData({ ...tiffinData, shortDescription: text })
-              }
-            />
-            <TextInput
-              style={styles.input}
-              placeholder="Enter Tiffin Price"
-              value={tiffinData.price}
-              onChangeText={(text) =>
-                setTiffinData({ ...tiffinData, price: text })
-              }
-              keyboardType="numeric"
-            />
+          if(tiffinData.availability){
+            if(!tiffinData.deliveryCharge.toString().length || !tiffinData.deliveryTimeHrs || !tiffinData.deliveryTimeMins){
+                Alert.alert("Please Fill All Delivery Details");
+                return
+            }
+          }
+        onEditTiffin(tiffinData);
+        onClose();
+    };
 
-            <View style={styles.pickerContainer}>
-              <Text style={styles.label}>Food Type</Text>
-              <RNPickerSelect
-                placeholder={{ label: "Select Food Type", value: null }}
-                value={tiffinData.foodType}
-                onValueChange={(value) =>
-                  setTiffinData({ ...tiffinData, foodType: value })
-                }
-                items={foodTypeOptions}
-                style={pickerSelectStyles}
-                useNativeAndroidPickerStyle={false}
-              />
-            </View>
+
+    const foodTypeOptions = ['Veg', 'Non-Veg'].map(value => ({ label: value, value: value }));
+    const tiffinTypeOptions = ['Lunch', 'Dinner'].map(value => ({ label: value, value: value }));
+    const hourOptions = Array.from({ length: 24 }, (_, i) => ({ label: i.toString().padStart(2, '0'), value: i.toString().padStart(2, '0') }));
+    const minuteOptions = ['00', '15', '30', '45'].map(value => ({ label: value, value: value }));
+
+    return (
+        <Modal
+            animationType="slide"
+            transparent={true}
+            visible={isVisible}
+            onRequestClose={onClose}
+        >
+            {item ? (
+                <View style={styles.modalContainer}>
+
+                    <View style={styles.modalContent}>
+                        <View style={styles.headerContainer}>
+                            <Text style={styles.modalTitle}>Edit {item.name}</Text>
+                            <View style={styles.closeButtonHeader}>
+                                <Icon
+                                    name="close"
+                                    type="ionicon"
+                                    style={styles.closeButton}
+                                    onPress={onClose}
+                                />
+                            </View>
+                        </View>
+
+
+                        <View style={styles.pickerContainer}>
+                            <Text style={styles.label}>Food Type</Text>
+                            <RNPickerSelect
+                                value={tiffinData.foodType}
+                                onValueChange={(value) => setTiffinData({ ...tiffinData, foodType: value })}
+                                items={foodTypeOptions}
+                                style={pickerSelectStyles}
+                                useNativeAndroidPickerStyle={false}
+                            />
+                        </View>
 
                         <View style={styles.pickerContainer}>
                             <Text style={styles.label}>Tiffin Type</Text>
                             <RNPickerSelect
-                                placeholder={{ label: 'Select Tiffin Type', value: null }}
                                 value={tiffinData.tiffinType}
                                 onValueChange={(value) => setTiffinData({ ...tiffinData, tiffinType: value })}
                                 items={tiffinTypeOptions}
@@ -128,12 +89,10 @@ const AddTiffinModal = ({ isVisible, onClose, onAddTiffin }) => {
                                 useNativeAndroidPickerStyle={false}
                             />
                         </View>
-              
                         <Text style={styles.label}>At What Time Would Tiffin Be Ready</Text>
                         <View style={styles.row}>
                             <View style={styles.pickerContainerHalf}>
                                 <RNPickerSelect
-                                    placeholder={{ label: 'Select Hours', value: null }}
                                     value={tiffinData.hours}
                                     onValueChange={(value) => setTiffinData({ ...tiffinData, hours: value })}
                                     items={hourOptions}
@@ -141,9 +100,8 @@ const AddTiffinModal = ({ isVisible, onClose, onAddTiffin }) => {
                                     useNativeAndroidPickerStyle={false}
                                 />
                             </View>
-                            <View style={styles.pickerContainerHalf}>
+                            <View style={styles.pickerContainerHalfLast}>
                                 <RNPickerSelect
-                                    placeholder={{ label: 'Select Minutes', value: null }}
                                     value={tiffinData.mins}
                                     onValueChange={(value) => setTiffinData({ ...tiffinData, mins: value })}
                                     items={minuteOptions}
@@ -162,10 +120,10 @@ const AddTiffinModal = ({ isVisible, onClose, onAddTiffin }) => {
                         </View>
                         {tiffinData.availability ? (
                             <>
+                                <Text style={styles.label}>Delivery Charge</Text>
                                 <TextInput
                                     style={styles.input}
-                                    placeholder="Enter Delivery Charge (if applicable)"
-                                    value={tiffinData.deliveryCharge.toString()}
+                                    value={tiffinData.deliveryCharge}
                                     onChangeText={(text) => setTiffinData({ ...tiffinData, deliveryCharge: text })}
                                     keyboardType="numeric"
                                 />
@@ -173,7 +131,6 @@ const AddTiffinModal = ({ isVisible, onClose, onAddTiffin }) => {
                                 <View style={styles.row}>
                                     <View style={styles.pickerContainerHalf}>
                                         <RNPickerSelect
-                                            placeholder={{ label: 'Select Hours', value: null }}
                                             value={tiffinData.deliveryTimeHrs}
                                             onValueChange={(value) => setTiffinData({ ...tiffinData, deliveryTimeHrs: value })}
                                             items={hourOptions}
@@ -181,9 +138,8 @@ const AddTiffinModal = ({ isVisible, onClose, onAddTiffin }) => {
                                             useNativeAndroidPickerStyle={false}
                                         />
                                     </View>
-                                    <View style={styles.pickerContainerHalf}>
+                                    <View style={styles.pickerContainerHalfLast}>
                                         <RNPickerSelect
-                                            placeholder={{ label: 'Select Minutes', value: null }}
                                             value={tiffinData.deliveryTimeMins}
                                             onValueChange={(value) => setTiffinData({ ...tiffinData, deliveryTimeMins: value })}
                                             items={minuteOptions}
@@ -194,31 +150,32 @@ const AddTiffinModal = ({ isVisible, onClose, onAddTiffin }) => {
                                 </View>
                             </>
                         ) : null}
-                
-                    </ScrollView>
-                    <View style = {styles.btnContainer}>
-                    <TouchableOpacity style={styles.submitButton} onPress={handleAddTiffin}>
-                        <Text style={styles.buttonText}>Add</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.closeButton} onPress={onClose}>
-                        <Text style={styles.buttonText}>Close</Text>
-                    </TouchableOpacity>
+
+                        <View style={styles.btnContainer}>
+                            <TouchableOpacity style={styles.submitButton} onPress={handleEditTiffin}>
+                                <Text style={styles.buttonText}>Edit</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.backButton} onPress={onBack}>
+                                <Text style={styles.buttonText}>Back</Text>
+                            </TouchableOpacity>
+                        </View>
                     </View>
                 </View>
-            </View>
+            ) : (
+                <Text>No Tiffin To Edit</Text>
+            )}
         </Modal>
     );
 };
 
-export default AddTiffinModal;
+export default EditTiffinModal2;
 
 const styles = StyleSheet.create({
-  modalContainer: {
-    flex: 1,
-    justifyContent: "flex-end",
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-  },
-
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
     modalContent: {
         backgroundColor: '#fff',
         borderTopLeftRadius: windowWidth * 0.05,
@@ -226,13 +183,27 @@ const styles = StyleSheet.create({
         padding: windowWidth * 0.03,
         width: '100%',
     },
-
+    headerContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: windowHeight * 0.01,
+    },
     modalTitle: {
         fontSize: windowWidth * 0.06,
         fontWeight: 'bold',
-        marginBottom: windowHeight * 0.01,
     },
-
+    closeButtonHeader: {
+        backgroundColor: '#FFFFFF',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: windowHeight * 0.04,
+        paddingHorizontal: windowWidth * 0.03,
+        fontSize: windowWidth * 0.07,
+    },
+    closeButton: {
+        fontSize: windowWidth * 0.08,
+    },
     input: {
         borderWidth: 1,
         borderColor: '#ccc',
@@ -241,32 +212,26 @@ const styles = StyleSheet.create({
         paddingHorizontal: windowWidth * 0.03,
         marginBottom: windowHeight * 0.01,
     },
-
     pickerContainer: {
         marginBottom: windowHeight * 0.01,
     },
-    
     pickerContainerHalf: {
         flex: 1,
-        marginRight: windowWidth * 0.01,  
+        marginRight: windowWidth * 0.02,
     },
-    
     pickerContainerHalfLast: {
         flex: 1,
-        marginRight: 0,  
+        marginRight: 0,
     },
-
     row: {
         flexDirection: 'row',
         justifyContent: 'space-between',
         marginBottom: windowHeight * 0.011,
     },
-
     label: {
         marginBottom: windowHeight * 0.005,
         fontSize: windowWidth * 0.04,
     },
-
     checkboxContainer: {
         flexDirection: 'row',
         alignItems: 'center',
@@ -280,11 +245,9 @@ const styles = StyleSheet.create({
         fontSize: windowWidth * 0.04,
         marginLeft: windowWidth * 0.025,
     },
-
     btnContainer: {
         alignItems: 'center'
     },
-
     submitButton: {
         backgroundColor: 'green',
         alignItems: 'center',
@@ -294,9 +257,8 @@ const styles = StyleSheet.create({
         borderRadius: windowWidth * 0.02,
         marginTop: windowHeight * 0.01,
     },
-
-    closeButton: {
-        backgroundColor: 'red',
+    backButton: {
+        backgroundColor: '#FF8C00',
         alignItems: 'center',
         justifyContent: 'center',
         height: windowHeight * 0.05,
@@ -304,7 +266,7 @@ const styles = StyleSheet.create({
         borderRadius: windowWidth * 0.02,
         marginTop: windowHeight * 0.01,
     },
-
+    
     buttonText: {
         color: '#fff',
         fontSize: windowWidth * 0.04,
