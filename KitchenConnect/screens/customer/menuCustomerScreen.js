8960a54dcfbce5be33ef8bbd2c2d98127097efbf
Mihir-Paija <mihir21102003@gmail.com>
2024-06-06 15:15:31 +0530

    subscription modal for customer created

--- KitchenConnect/screens/customer/menuCustomerScreen.js
+++ KitchenConnect/screens/customer/menuCustomerScreen.js
@@ -21,63 +21,68 @@ import HeaderMenuCustomer from "../../components/customer/menuScreenHeader";
 import TiffinComponent from "../../components/customer/tiffinComponent";
 import BackButtonComponent from "../../components/shared/BackButton";
 import { AuthContext } from "@/context/authContext";
-import SortModalTiffinCustomer from "../../components/customer/SortModalTiffinCustome";
-import FilterModalTiffinCustomer from "../../components/customer/FilterModalTiffinCustomer";
+import SubscriptionModalCustomer from "../../components/customer/subscriptionModalCustomer";
 import { getMenuCustomer } from "@/utils/APIs/customerApi";
 import LoadingScreen from "@/screens/shared/loadingScreen";
 import Collapsible from "react-native-collapsible";
 import { FontAwesome, Feather } from "@expo/vector-icons";
 import MenuItemComponent from "../../components/customer/menuItemComponent";
 import DaysScrollView from "../../components/customer/dayScrollViewMenuCustomer";
+import SubmitButton from "../../components/shared/forms/submitButton";
 
 const MenuCustomerScreen = ({ navigation, route }) => {
   //dummyy data
-  //   const menuData = {
+  //   const menuDummyList = {
   //     Mon: [
-  //       { name: "Sev Tameta", quantity: "250", unit: "g", id: 1 },
-  //       { name: "Butter Roti", quantity: "4", unit: "pcs", id: 2 },
-  //       { name: "Jeera Rice", quantity: "250", unit: "g", id: 3 },
-  //       { name: "Dal", quantity: "250", unit: "ml", id: 4 },
-  //       { name: "Butter Milk", quantity: "200", unit: "ml", id: 5 },
+  //       { itemName: "Sev Tameta", quantity: "250", unit: "g", _id: 1 },
+  //       { itemName: "Butter Roti", quantity: "4", unit: "pcs", _id: 2 },
+  //       { itemName: "Jeera Rice", quantity: "250", unit: "g", _id: 3 },
+  //       { itemName: "Dal", quantity: "250", unit: "ml", _id: 4 },
+  //       { itemName: "Butter Milk", quantity: "200", unit: "ml", _id: 5 },
+  //       { itemName: "Sev Tameta", quantity: "250", unit: "g", _id: 6 },
+  //       { itemName: "Butter Roti", quantity: "4", unit: "pcs", _id: 7 },
+  //       { itemName: "Jeera Rice", quantity: "250", unit: "g", _id: 8 },
+  //       { itemName: "Dal", quantity: "250", unit: "ml", _id: 9 },
+  //       { itemName: "Butter Milk", quantity: "200", unit: "ml", _id: 10 },
   //     ],
   //     Tue: [
-  //       { name: "Dal Tadka", quantity: "250", unit: "g", id: 1 },
-  //       { name: "Steamed Rice", quantity: "200", unit: "g", id: 2 },
-  //       { name: "Chapati", quantity: "4", unit: "pcs", id: 3 },
-  //       { name: "Salad", quantity: "100", unit: "g", id: 4 },
-  //       { name: "Sweet", quantity: "1", unit: "pcs", id: 5 },
+  //       { itemName: "Dal Tadka", quantity: "250", unit: "g", _id: 1 },
+  //       { itemName: "Steamed Rice", quantity: "200", unit: "g", _id: 2 },
+  //       { itemName: "Chapati", quantity: "4", unit: "pcs", _id: 3 },
+  //       { itemName: "Salad", quantity: "100", unit: "g", _id: 4 },
+  //       { itemName: "Sweet", quantity: "1", unit: "pcs", _id: 5 },
   //     ],
   //     Wed: [
-  //       { name: "Aloo Paratha", quantity: "2", unit: "pcs", id: 1 },
-  //       { name: "Curd", quantity: "100", unit: "g", id: 2 },
-  //       { name: "Pickle", quantity: "50", unit: "g", id: 3 },
-  //       { name: "Sweet", quantity: "1", unit: "pcs", id: 4 },
+  //       { itemName: "Aloo Paratha", quantity: "2", unit: "pcs", _id: 1 },
+  //       { itemName: "Curd", quantity: "100", unit: "g", _id: 2 },
+  //       { itemName: "Pickle", quantity: "50", unit: "g", _id: 3 },
+  //       { itemName: "Sweet", quantity: "1", unit: "pcs", _id: 4 },
   //     ],
   //     Thu: [
-  //       { name: "Chole", quantity: "250", unit: "g", id: 1 },
-  //       { name: "Pulao", quantity: "200", unit: "g", id: 2 },
-  //       { name: "Salad", quantity: "100", unit: "g", id: 3 },
-  //       { name: "Sweet", quantity: "1", unit: "pcs", id: 4 },
+  //       { itemName: "Chole", quantity: "250", unit: "g", _id: 1 },
+  //       { itemName: "Pulao", quantity: "200", unit: "g", _id: 2 },
+  //       { itemName: "Salad", quantity: "100", unit: "g", _id: 3 },
+  //       { itemName: "Sweet", quantity: "1", unit: "pcs", _id: 4 },
   //     ],
   //     Fri: [
-  //       { name: "Paneer Butter Masala", quantity: "250", unit: "g", id: 1 },
-  //       { name: "Naan", quantity: "2", unit: "pcs", id: 2 },
-  //       { name: "Jeera Rice", quantity: "200", unit: "g", id: 3 },
-  //       { name: "Salad", quantity: "100", unit: "g", id: 4 },
-  //       { name: "Sweet", quantity: "1", unit: "pcs", id: 5 },
+  //       { itemName: "Paneer Butter Masala", quantity: "250", unit: "g", _id: 1 },
+  //       { itemName: "Naan", quantity: "2", unit: "pcs", _id: 2 },
+  //       { itemName: "Jeera Rice", quantity: "200", unit: "g", _id: 3 },
+  //       { itemName: "Salad", quantity: "100", unit: "g", _id: 4 },
+  //       { itemName: "Sweet", quantity: "1", unit: "pcs", _id: 5 },
   //     ],
   //     Sat: [
-  //       { name: "Kadhi Chawal", quantity: "250", unit: "g", id: 1 },
-  //       { name: "Mix Veg", quantity: "200", unit: "g", id: 2 },
-  //       { name: "Chapati", quantity: "4", unit: "pcs", id: 3 },
-  //       { name: "Salad", quantity: "100", unit: "g", id: 4 },
-  //       { name: "Sweet", quantity: "1", unit: "pcs", id: 5 },
+  //       { itemName: "Kadhi Chawal", quantity: "250", unit: "g", _id: 1 },
+  //       { itemName: "Mix Veg", quantity: "200", unit: "g", _id: 2 },
+  //       { itemName: "Chapati", quantity: "4", unit: "pcs", _id: 3 },
+  //       { itemName: "Salad", quantity: "100", unit: "g", _id: 4 },
+  //       { itemName: "Sweet", quantity: "1", unit: "pcs", _id: 5 },
   //     ],
   //     Sun: [
-  //       { name: "Rajma Chawal", quantity: "250", unit: "g", id: 1 },
-  //       { name: "Raita", quantity: "100", unit: "g", id: 2 },
-  //       { name: "Pickle", quantity: "50", unit: "g", id: 3 },
-  //       { name: "Sweet", quantity: "1", unit: "pcs", id: 4 },
+  //       { itemName: "Rajma Chawal", quantity: "250", unit: "g", _id: 1 },
+  //       { itemName: "Raita", quantity: "100", unit: "g", _id: 2 },
+  //       { itemName: "Pickle", quantity: "50", unit: "g", _id: 3 },
+  //       { itemName: "Sweet", quantity: "1", unit: "pcs", _id: 4 },
   //     ],
   //   };
 
@@ -89,10 +94,17 @@ const MenuCustomerScreen = ({ navigation, route }) => {
   const [loading, setLoading] = useState(true);
   const [selectedDay, setSelectedDay] = useState("Mon");
   const [menuList, setMenuList] = useState([]);
+  const [subscriptionModalVisible, setSubscriptionModalVisible] =
+    useState(false);
 
   //functions
   const backHandler = () => {
     navigation.goBack();
+  };
+
+  const subscriptionHandler = () => {
+    console.log("click on subscription");
+    setSubscriptionModalVisible(true);
   };
 
   const onDayChange = (day) => {
@@ -109,7 +121,7 @@ const MenuCustomerScreen = ({ navigation, route }) => {
       }, {});
 
       setMenuList(formattedMenu);
-      console.log("menuList:", formattedMenu);
+      //   console.log("menuList:", formattedMenu);
     } catch (error) {
       console.error("Failed to fetch menu:", error);
     } finally {
@@ -143,18 +155,29 @@ const MenuCustomerScreen = ({ navigation, route }) => {
                   </Text>
                 </View>
               ) : (
-                <FlatList
-                  data={menuList[selectedDay]}
-                  keyExtractor={(item) => item._id.toString()}
-                  renderItem={({ item }) => (
-                    <MenuItemComponent
-                      name={item.itemName}
-                      quantity={item.quantity}
-                      unit={item.unit}
-                    />
-                  )}
-                  contentContainerStyle={styles.mealList}
-                />
+                <>
+                  <FlatList
+                    data={menuList[selectedDay]}
+                    keyExtractor={(item) => item._id.toString()}
+                    renderItem={({ item }) => (
+                      <MenuItemComponent
+                        name={item.itemName}
+                        quantity={item.quantity}
+                        unit={item.unit}
+                      />
+                    )}
+                    contentContainerStyle={styles.mealList}
+                  />
+                  <SubmitButton
+                    btnTitle={"Subscriptions"}
+                    handleSubmitBtn={subscriptionHandler}
+                    loading={loading}
+                  />
+                  <SubscriptionModalCustomer
+                    visible={subscriptionModalVisible}
+                    onClose={() => setSubscriptionModalVisible(false)}
+                  />
+                </>
               )}
               {/* <Text>MenuCustomerScreen</Text>
             <Text>{JSON.stringify(tiffin)}</Text> */}
