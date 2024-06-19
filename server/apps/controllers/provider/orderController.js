0760e9ba52893b9b4b15cafaf1120a5697b5019b
Dhruv Shah <94975058+DhruvShah87@users.noreply.github.com>
2024-06-20 01:01:54 +0530

    Created Model and API for One Time Order

--- server/apps/controllers/provider/orderController.js
+++ server/apps/controllers/provider/orderController.js
@@ -1,10 +1,20 @@
 import subscriber from '../../models/subscriberModel.js'
+import tiffins from '../../models/tiffinModel.js';
+import order from '../../models/orderModel.js';
 import mongoose from 'mongoose';
 
 export const getOrders = async (req, res) => {
 
     try {
         const userID = req.user._id;
+
+        const tiffin = await tiffins.find({providerID: new mongoose.Types.ObjectId(userID) })
+
+        const tiffinMap = new Map();
+
+        for(value of tiffins){
+          tiffinMap.set(value._id, [value.name, value.tiffinType])
+        }
 
         const subscribers = await subscriber.find({ providerID: new mongoose.Types.ObjectId(userID) })
 
@@ -25,9 +35,15 @@ export const getOrders = async (req, res) => {
         }
 
         for (const subscriber of subscribers) {
-            const { tiffinName, tiffinType, noOfTiffins, startDate, endDate, accepted } = subscriber._doc;
+            const { tiffinID, noOfTiffins, startDate, endDate, status } = subscriber._doc;
+            const tiffinName = tiffinMap.get(tiffinID)[0]
+            const tiffinType = tiffinMap.get(tiffinID)[1]
 
-            if (new Date(startDate) <= currentDate && currentDate <= new Date(endDate) && accepted) {
+            subscriber._doc.tiffinName = tiffinName
+            subscriber._doc.tiffinType = tiffinType
+            subscriber._doc.title = 'Subscription'
+
+            if (new Date(startDate) <= currentDate && currentDate <= new Date(endDate) && status === 'Current') {
 
                 const arr = tiffinType === 'Lunch' ? lunch : dinner;
 
@@ -48,6 +64,38 @@ export const getOrders = async (req, res) => {
             }
         };
 
+        const orders = await order.find({ providerID: new mongoose.Types.ObjectId(userID) })
+
+        const toDate = currentDate.setHours(0, 0, 0, 0);
+
+        for(value of orders){
+            if(value.createdAt >= toDate && value.status === 'Accepted'){
+                const tiffinID = value.tiffinID
+                const tiffinName = tiffinMap.get(tiffinID)[0]
+                const tiffinType = tiffinMap.get(tiffinID)[1]
+                value.tiffinName = tiffinName
+                value.tiffinType = tiffinType
+                value.title = 'One Time'
+                const arr = tiffinType === 'Lunch' ? lunch : dinner;
+
+                const index = findTiffinIndex(arr, tiffinName);
+
+                if (index === -1) {
+                    insertInSortedOrder(arr, {
+                        tiffinName: tiffinName,
+                        number: noOfTiffins,
+                        subscribers: [subscriber]
+                    });
+                } else {
+                    arr[index].number += noOfTiffins;
+                    arr[index].subscribers.push(subscriber._doc);
+                    const [updatedItem] = arr.splice(index, 1);
+                    insertInSortedOrder(arr, updatedItem);
+                }
+            }
+
+        }
+
         return res.status(200).send({
             lunch: lunch,
             dinner: dinner
@@ -59,4 +107,75 @@ export const getOrders = async (req, res) => {
             message: `Internal Server Error`
         })
     }
+}
+
+export const getPendingOrders = async(req, res) =>{
+    try {
+        const userID = req.user._id;
+
+        const tiffin = await tiffins.find({providerID: new mongoose.Types.ObjectId(userID) })
+
+        const tiffinMap = new Map();
+
+        for(value of tiffins){
+          tiffinMap.set(value._id, [value.name, value.tiffinType])
+        } 
+
+        const orders = await order.find({ providerID: new mongoose.Types.ObjectId(userID) })
+
+        const toDate = currentDate.setHours(0, 0, 0, 0);
+        const pendingOrders = []
+
+        for(value of orders){
+            if(value.createdAt >= toDate && value.status === 'Pending'){
+                const tiffinID = value.tiffinID
+                const tiffinName = tiffinMap.get(tiffinID)[0]
+                const tiffinType = tiffinMap.get(tiffinID)[1]
+                value.tiffinName = tiffinName
+                value.tiffinType = tiffinType
+                pendingOrders.push(value)
+            }
+        }
+
+        return req.status(200).send({
+            pendingOrders: pendingOrders
+        })
+    } catch (error) {
+        console.log('Error in Fetching Pending Orders ', error)
+        return res.status(500).send({
+            message: `Internal Server Error`
+        })
+    }
+}
+
+
+export const decideOrderStatus = async(req, res) =>{
+    try {
+        const userID = req.user._id;
+        const {orderID} = req.params;
+
+        const {status} = req.body
+
+        const current = await order.findById(orderID)
+
+        if (!current)
+            return res.status(404).send({
+                message: `Order Not Found`
+            })
+
+        current.status = status;
+
+        await current.save();
+
+        return res.status(200).send({
+            message: `Updated Successfully`
+        })
+
+    } catch (error) {
+        console.log('Error in Deciding Status - Orders ', error)
+        return res.status(500).send({
+            message: `Internal Server Error`
+        })
+    }
+
 }
